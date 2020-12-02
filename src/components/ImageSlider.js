b761be89730bc9f9af9b1f1cec12d45d56da634e
foday <foday_s@yahoo.com>
2020-12-02 12:07:14 -0600

    Display-Modified ImageSlider.js file
    Description: Insert {useState} after 'import React,'then I add 'const [current, setCurrent] = useState(0)' which sets the useStates value to 0. The useState function is a built in hook that can be imported from the react package. It allows you to add state to your functional components. Using the useState hook inside a function component, you can create a piece of state without switching to class components.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { SliderData } from './SliderData'
 
 const ImageSlider = () => {
+const [current, setCurrent ] = useState(0)
+//const length = slides.length
+
+
     return (
         <div>
             {
             SliderData.map((slide, index) => 
-            {return <img src={slide.img} alt="travel image" />;}
+            {return <img src={slide.image} alt="travel image" />;}
             )
             }
         </div>
