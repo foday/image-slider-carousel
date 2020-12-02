b707623d6933dac73878191cec3d33767224fd6b
foday <foday_s@yahoo.com>
2020-12-02 10:48:59 -0600

    Add an Array of image urls address to SliderData.js and typed rafce then pressed tab or enter to populate ImageSlider.js file
    In the Imageslider file I simply typed 'image' just to put something there then I modified the App.js file by adding the ImageSlider component and in doing so I test to see if the ImageSlider component works

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1573950940509-d924ee3fd345?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1096&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1591293836027-e05b48473b67?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
+    }, 
+    {
+        image: 'https://images.unsplash.com/photo-1553440569-bcc63803a83d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8Y2Fyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
+    }
+]
