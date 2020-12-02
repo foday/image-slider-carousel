92623fb852e7931ab8287f732145386f03ff53ed
foday <foday_s@yahoo.com>
2020-12-02 15:19:06 -0600

    .slider class in css being used to position all the pics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
 
 function App() {
   return (
-    <ImageSlider />
+    <ImageSlider slides={SliderData}/>
   );
 }
 
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,19 +1,23 @@
 import React, {useState} from 'react'
 import { SliderData } from './SliderData'
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 
-const ImageSlider = () => {
+const ImageSlider = ({ slides }) => {
 const [current, setCurrent ] = useState(0)
-//const length = slides.length
+const length = slides.length
 
 
     return (
-        <div>
+        <section className="slider">
+            <FaArrowAltCircleLeft className="left-arrow" />
+            <FaArrowAltCircleRight className="right-arrow" />
             {
             SliderData.map((slide, index) => 
-            {return <img src={slide.image} alt="travel image" />;}
+            {return <img src={slide.image} alt="travel image" className='image'/>;}
             )
             }
-        </div>
+        </section>
+        
     );
 };
 
