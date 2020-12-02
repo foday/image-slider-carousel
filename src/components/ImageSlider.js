2220df74966b971d3984288467b66ef9ae641371
foday <foday_s@yahoo.com>
2020-12-02 15:39:49 -0600

    modified ImageSlider.js file by adding conditions and onClick

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -6,11 +6,25 @@ const ImageSlider = ({ slides }) => {
 const [current, setCurrent ] = useState(0)
 const length = slides.length
 
+const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+};
+
+const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+}
+
+console.log(current);
+
+if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+}
+
 
     return (
         <section className="slider">
-            <FaArrowAltCircleLeft className="left-arrow" />
-            <FaArrowAltCircleRight className="right-arrow" />
+            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
             {
             SliderData.map((slide, index) => 
             {return <img src={slide.image} alt="travel image" className='image'/>;}
